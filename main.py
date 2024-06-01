dd5bad09cf5a67b51f232be03922927cc7c197eb
Roderick002 <104246601+Roderick002@users.noreply.github.com>
2024-06-01 16:14:58 +0000

    Setup Pages

--- main.py
+++ main.py
@@ -8,5 +8,30 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
+
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+
+@app.route('/testimonials')
+def testimonials():
+    return render_template('testimonials.html')
+
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('testimonials.html')
+
+
+@app.route('/about_us')
+def about_us():
+    return render_template('testimonials.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
